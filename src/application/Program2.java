7cc145becd1366533358d5183df914cf675fb099
Matheus <matheus-mv@outlook.com>
2025-07-09 15:08:27 -0300

    department delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -8,6 +10,8 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("\n=== TEST 1: deparment insert ===");
@@ -15,6 +19,13 @@ public class Program2 {
 		departmentDao.insert(newDepartment);
 		
 		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
+		System.out.println("\n=== TEST 2: department delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		sc.close();
 	}
 
 }
